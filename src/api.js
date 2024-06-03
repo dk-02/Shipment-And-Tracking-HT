ebdf6cd23be279357b1fdacd97ca764f1b0801db
Dora <dora.kasik@fer.hr>
2024-06-03 20:44:59 +0200

    Add axios data fetching

--- src/api.js
+++ src/api.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8018/shipmentTracking/v1',
+    baseURL: 'http://localhost:8017/shipmentTracking/v1',
     headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:3000'
+        'Content-Type': 'application/json'
     }
 });
 
